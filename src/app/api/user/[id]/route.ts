135fc0bb97122730888abe5d3f458f9f294b887c
papahmwe <doublehmwenetmsi123@gmail.com>
2023-10-06 18:25:51 +0630

    nextauth-nextjs

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,36 @@
+import { verifyJwt } from "@/lib/jwt";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const accessToken = request.headers.get("authorization");
+
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(
+      JSON.stringify({
+        error: "unauthorized",
+      }),
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userPsots = await prisma.post.findMany({
+    where: {
+      authorId: +params.id,
+    },
+    include: {
+      author: {
+        select: {
+          email: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  return new Response(JSON.stringify(userPsots));
+}
